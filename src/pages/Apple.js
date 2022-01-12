71a15d2accecd9d694f4bc9b6218ac8a84c14ebf
gXK07 <80717523+gXK07@users.noreply.github.com>
2022-01-12 22:35:22 +0100

    homepage

--- src/pages/Apple.js
+++ src/pages/Apple.js
@@ -1,51 +1,110 @@
-import One_PA from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/One_PA.jpeg"
-import hero_iphone_13__f194u1rdooeq_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/hero_iphone_13__f194u1rdooeq_large.jpeg"
-import hero_watch__f05go333pz2i_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/hero_watch__f05go333pz2i_large.jpeg"
-import promo_el_deafo__ge0lcfxsw3ue_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/promo_el_deafo__ge0lcfxsw3ue_large.jpeg"
-import tile_airpods__fuv6vgn42y2q_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_airpods__fuv6vgn42y2q_large.jpeg"
-import tile_apple_card__cwonh0wb00om_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_apple_card__cwonh0wb00om_large.jpeg"
-import tile_applefitness__fdk8xvz0dvu6_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_applefitness__fdk8xvz0dvu6_large.jpeg"
-import tile_homepod_mini__b73w4z3ljymu_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_homepod_mini__b73w4z3ljymu_large.jpeg"
-import tile_macbookpro__cx0ur0qg9biq_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_macbookpro__cx0ur0qg9biq_large.jpeg"
+import One_PA from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/One_PA.jpeg";
+import hero_iphone_13__f194u1rdooeq_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/hero_iphone_13__f194u1rdooeq_large.jpeg";
+import hero_watch__f05go333pz2i_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/hero_watch__f05go333pz2i_large.jpeg";
+import promo_el_deafo__ge0lcfxsw3ue_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/promo_el_deafo__ge0lcfxsw3ue_large.jpeg";
+import tile_airpods__fuv6vgn42y2q_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_airpods__fuv6vgn42y2q_large.jpeg";
+import tile_apple_card__cwonh0wb00om_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_apple_card__cwonh0wb00om_large.jpeg";
+import tile_applefitness__fdk8xvz0dvu6_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_applefitness__fdk8xvz0dvu6_large.jpeg";
+import tile_homepod_mini__b73w4z3ljymu_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_homepod_mini__b73w4z3ljymu_large.jpeg";
+import tile_macbookpro__cx0ur0qg9biq_large from "/Users/gregoire/Documents/Projet code/Odin_project/portfolio/apple-fake/src/Assets/home/tile_macbookpro__cx0ur0qg9biq_large.jpeg";
 
 const Panel = (props) => {
-    if (props.type === "OneLinePanel")
-        {
-            return <div className="OneLinePanel">
-                <div className="boxContent">
-                    <div className="Links">Learn more</div>
-                    <img src={props.img} alt={props.img}></img>
-                </div>
-            </div>
-        }
-        else return <div className="TwoLinePanel">
-            <img src={props.imgs[0]} alt={props.imgs[0]}></img>
-            <img src={props.imgs[1]} alt={props.imgs[1]}></img>
-        </div>
+  let links = [];
+  if (props.links.length > 0) {
+    for (let i = 0; i < props.links.length; i++) {
+      links.push(
+        <a key={i} className="link">
+          {props.links[i]}
+        </a>
+      );
     }
-
-
+  }
+  return (
+    <div className="Panel" id={props.identity}>
+      <div className="InfoBox">
+        <div className="Title">{props.Title}</div>
+        <div className="Description">{props.Description}</div>
+        <div className="LinksBox">{links}</div>
+      </div>
+      <img src={props.img} alt={props.img}></img>
+    </div>
+  );
+};
 
 const Apple = () => {
-    return <div className="Apple">
-        <div className="top">
-            <Panel type="OneLinePanel" img={One_PA}></Panel>
-            <Panel type="OneLinePanel" img={hero_iphone_13__f194u1rdooeq_large}></Panel>
-            <Panel type="OneLinePanel" img={hero_watch__f05go333pz2i_large}></Panel>
-            <Panel type="TwoLinePanel" imgs={[
-                tile_applefitness__fdk8xvz0dvu6_large, tile_macbookpro__cx0ur0qg9biq_large
-                ]}>
-            </Panel>
-            <Panel type="TwoLinePanel" imgs={[
-                tile_airpods__fuv6vgn42y2q_large, tile_homepod_mini__b73w4z3ljymu_large
-                ]}>
-            </Panel>
-            <Panel type="TwoLinePanel" imgs={[
-                tile_apple_card__cwonh0wb00om_large, promo_el_deafo__ge0lcfxsw3ue_large
-                ]}>
-            </Panel>
-        </div>
+  return (
+    <div className="Apple">
+      <Panel
+        identity="iPhonePro"
+        Title="iPhone 13 pro"
+        Description="Oh. So. Pro."
+        links={["Learn more", "Buy"]}
+        img={One_PA}
+      ></Panel>
+      <Panel
+        identity="iPhone"
+        Title="iPhone 13"
+        Description="Your New Superpower."
+        links={["Learn more", "Buy"]}
+        img={hero_iphone_13__f194u1rdooeq_large}
+      ></Panel>
+      <Panel
+        identity="watch"
+        Title="New WATCH SERIES 7"
+        Description="Introducing our largest display yet."
+        links={["Learn more", "Buy"]}
+        img={hero_watch__f05go333pz2i_large}
+      ></Panel>
+      <div className="TwoPanelsOneline">
+        <Panel
+          identity="fitness"
+          Title="Fitness +"
+          Description="More ways to move. Now with Time to Run and Collections."
+          links={["Learn more", "Try it free"]}
+          img={tile_applefitness__fdk8xvz0dvu6_large}
+        ></Panel>
+        <Panel
+          identity="macPro"
+          Title="MacBook Pro"
+          Description="Supercharged for Pros"
+          links={["Learn more", "Buy"]}
+          img={tile_macbookpro__cx0ur0qg9biq_large}
+        ></Panel>
+      </div>
+      <div className="TwoPanelsOneline">
+        <Panel
+          identity="airPods"
+          Title="AirPods"
+          Description="All new with spatial audio"
+          links={["Learn more", "Buy"]}
+          img={tile_airpods__fuv6vgn42y2q_large}
+        ></Panel>
+        <Panel
+          identity="homePods"
+          Title="HomePods Mini"
+          Description="You’ve never heard color like this."
+          links={["Learn more", "Buy"]}
+          img={tile_homepod_mini__b73w4z3ljymu_large}
+        ></Panel>
+      </div>
+      <div className="TwoPanelsOneline">
+        <Panel
+          identity="card"
+          Title="Card"
+          Description="Save 5% on Apple products with a new Apple Card through January 31."
+          links={["Learn more", "Apply now"]}
+          img={tile_apple_card__cwonh0wb00om_large}
+        ></Panel>
+        <Panel
+          identity="deafo"
+          Title="El deafo"
+          Description="Find your inner Hero."
+          links={["Stream now"]}
+          img={promo_el_deafo__ge0lcfxsw3ue_large}
+        ></Panel>
+      </div>
     </div>
-}
+  );
+};
 
-export default Apple
+export default Apple;
